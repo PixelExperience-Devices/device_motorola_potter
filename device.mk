786dda94c6e22d103bcc88b98bc1454d5a294f50
Vachounet <vachounet@live.fr>
2017-08-23 13:00:12 +0200

    potter: Build DRM HAL
    Change-Id: Id7cb3c2e3d21e3b13faf5400d1751bd7d652ea6d

--- device.mk
+++ device.mk
@@ -198,6 +198,10 @@ PRODUCT_PACKAGES += \
     lights.msm8953 \
     android.hardware.light@2.0-impl
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl \
+
 # Media
 PRODUCT_PACKAGES += \
     libc2dcolorconvert
