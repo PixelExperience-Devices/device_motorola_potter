f26395d1b0ef30dc45b73834df2b419cb684c584
Vachounet <vachounet@live.fr>
2017-11-19 15:17:14 +0100

    potter: Bring up 32_64_bit support
    * Do not use default core_64bit product, and ship a custom one to default run in 32bit mode
    
    Change-Id: I0e291a6d4a95efdfdacfa3fe8427863a86cf7962

--- ev.mk
+++ ev.mk
@@ -15,10 +15,9 @@
 # limitations under the License.
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, device/moto/potter/full_potter.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, device/motorola/potter/full_potter.mk)
+$(call inherit-product, device/motorola/potter/core_32_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # for specific
 $(call inherit-product, vendor/moto/potter/potter-vendor.mk)
