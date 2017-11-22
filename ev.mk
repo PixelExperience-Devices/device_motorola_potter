55b31029e4fe011aaebbd39052490f92dee96403
Vachounet <vachounet@live.fr>
2017-11-22 20:34:04 +0100

    potter: update 64bits flags
    Change-Id: I51be498ef175dca9a678fbb937d51cc75f7780fa

--- ev.mk
+++ ev.mk
@@ -16,7 +16,7 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, device/moto/potter/full_potter.mk)
-
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
