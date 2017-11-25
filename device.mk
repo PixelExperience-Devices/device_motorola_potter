514a84dc2379ef5adf79bb77ed8e9100d0ace64f
Vachounet <vachounet@live.fr>
2017-11-25 10:38:27 +0100

    potter: revert some binderized services

--- device.mk
+++ device.mk
@@ -133,11 +133,8 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
-    android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
-    android.hardware.configstore@1.0-service \
     hwcomposer.msm8953 \
     memtrack.msm8953 \
     libgenlock \
@@ -147,8 +144,7 @@ PRODUCT_PACKAGES += android.hardware.media.omx@1.0-impl
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \
-    android.hardware.renderscript@1.0-impl \
-    android.hardware.renderscript@1.0-service
+    android.hardware.renderscript@1.0-impl
 
 # DRM
 PRODUCT_PACKAGES += \
@@ -228,10 +224,6 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service \
     lights.msm8953
 
-
-PRODUCT_PACKAGES += \
-    vendor.display.color@1.0-service \
-    vendor.display.color@1.0-impl
 
 # DRM
 PRODUCT_PACKAGES += \
