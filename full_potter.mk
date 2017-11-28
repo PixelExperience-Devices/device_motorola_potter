ArrowOS-Devices/android_device_motorola_potter

potter: Optimize makefiles

full_potter.mk
@@ -14,10 +14,19 @@
 # limitations under the License.
 #
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, device/motorola/potter/core_32_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 # Inherit from potter device
 $(call inherit-product, device/motorola/potter/device.mk)
+
+# Dexpreopt fixes
 $(call inherit-product, device/motorola/potter/dexpreopt.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := potter
 PRODUCT_NAME := full_potter
+
+# for specific
+$(call inherit-product, vendor/motorola/potter/potter-vendor.mk)

PixelExperience-Devices/device_motorola_potter

Change-Id: I25a4df2b8a259412f66c3375c921f0fcef8706b4

full_potter.mk
@@ -14,6 +14,15 @@
 # limitations under the License.
 #
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, device/moto/potter/core_32_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 # Inherit from potter device
-$(call inherit-product, device/motorola/potter/device.mk)
-$(call inherit-product, device/motorola/potter/dexpreopt.mk)
+$(call inherit-product, device/moto/potter/device.mk)
+
+# Dexpreopt fixes
+$(call inherit-product, device/moto/potter/dexpreopt.mk)
+
+# for specific
+$(call inherit-product, vendor/moto/potter/potter-vendor.mk)