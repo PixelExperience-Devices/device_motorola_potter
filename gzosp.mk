7054f916587bbb442fea0a534f39bbc048f5a03e
GtrCraft <nickvanbokhorst@hotmail.com>
2017-12-07 10:12:32 +0100

    Add me as maintainer

--- gzosp.mk
+++ gzosp.mk
@@ -36,4 +36,5 @@ PRODUCT_SYSTEM_PROPERTY_BLACKLIST := ro.product.model
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT=motorola/potter/potter:7.0/NPNS25.137-33-11/11:user/release-keys \
     PRIVATE_BUILD_DESC="potter-7.0/NPNS25.137-33-11/11:user/release-keys" \
-    PRODUCT_NAME="Moto G5 Plus"
+    PRODUCT_NAME="Moto G5 Plus" \
+    DEVICE_MAINTAINERS="Nick van Bokhorst (GtrCraft)"
