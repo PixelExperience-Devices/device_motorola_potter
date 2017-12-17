64fd3e400ba4f73ae49f7789286139e916891ae2
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-12-17 18:39:37 -0200

    potter: Use Pixel build fingerprint to bypass safetynet

--- init/init_potter.cpp
+++ init/init_potter.cpp
@@ -72,7 +72,7 @@ void vendor_load_properties()
 
     // fingerprint
     property_override("ro.build.description", "potter-7.0/NPNS25.137-33-11/11:user/release-keys");
-    property_override("ro.build.fingerprint", "motorola/potter/potter:7.0/NPNS25.137-33-11/11:user/release-keys");
+    property_override("ro.build.fingerprint", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
 
     property_set("ro.product.model", sku.c_str());
 
