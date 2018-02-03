7b9522aa1b22e81bfa9a0cb326468e2b2a8c22a0
Rashed Abdel-Tawab <rashed@linux.com>
2018-02-02 19:39:50 -0800

    potter: Set shipping API level via inherited makefile
     * PRODUCT_SHIPPING_API_LEVEL is set there
    
    Change-Id: I8adecfbaafb538277f6451a5dbc8abc279bf0c15

--- full_potter.mk
+++ full_potter.mk
@@ -18,6 +18,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
 # Inherit from potter device
 $(call inherit-product, device/moto/potter/device.mk)
