503cc17302bbf3083c05ec073b1e07f8e5dc3d02
Vachounet <vachounet@live.fr>
2018-02-07 20:43:06 +0100

    potter: update GPS HAL
    * LA.UM.6.5.r1-04000-8x96.0
    
    Change-Id: I53666d7eb27d350c6a4893ad37d2a69b16fceb7e

--- device.mk
+++ device.mk
@@ -175,18 +175,15 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
     android.hardware.gatekeeper@1.0-service
 
-# GNSS HAL
-PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl
-
 # GPS
 PRODUCT_PACKAGES += \
-    libgps.utils \
-    gps.default \
     libgnss \
-    liblocation_api \
+    libgnsspps \
     android.hardware.gnss@1.0-impl-qti \
-    libloc_api-rpc-qc
+    android.hardware.gnss@1.0-impl \
+    android.hardware.gnss@1.0-service-qti \
+    libqsap_sdk \
+    libqsap_shim
 
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0
