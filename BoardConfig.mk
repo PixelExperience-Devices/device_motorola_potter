675664f61c3358980b886aeae38f798bcd5a6fc1
William Bellavance <flintman@flintmancomputers.com>
2018-04-09 10:35:03 -0400

    Clang me please
    Update to compile kernel with Clang
    
    Change-Id: I97ac01d188471ccc3417c45075941c6687e37c01

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -92,8 +92,7 @@ TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_CONFIG := potter_defconfig
 TARGET_KERNEL_SOURCE := kernel/moto/msm8953
 
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-androidkernel-
+TARGET_KERNEL_CLANG_COMPILE := true
 
 # Audio
 AUDIO_FEATURE_ENABLED_ALAC_OFFLOAD := true
