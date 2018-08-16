75a1b0fe07b5ec1b363688386b602ae4ce8b88ed
Bruno Martins <bgcngm@gmail.com>
2018-08-16 15:19:28 +0100

    potter: Switch to common basic USB HAL
    Partially reverts 2dfaf12dd4bca68e8cd0a000422c3dbd96b7432b
    
    Change-Id: I0e0809a2338f006063c5160bb7aec36a555cebab

--- device.mk
+++ device.mk
@@ -386,7 +386,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.potter
+    android.hardware.usb@1.0-service.basic
 
 # Vibrator
 PRODUCT_PACKAGES += \
