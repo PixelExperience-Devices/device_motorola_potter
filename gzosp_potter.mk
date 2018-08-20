ee20ffd46dd6501bb32acac4f0afbc2cf76aad2d
GtrCraft <nickvanbokhorst@hotmail.com>
2018-08-20 09:17:02 +0200

    potter: Consider gzosp_potter product via the AOSP way

--- gzosp_potter.mk
+++ gzosp_potter.mk
@@ -1,18 +1,18 @@
 #
-# Copyright (C) 2015 The CyanogenMod Project
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
@@ -21,9 +21,27 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from potter device
 $(call inherit-product, device/motorola/potter/device.mk)
 
-# Device identifier. This must come after all inclusions
+# Inherit some common GZOSP stuff.
+$(call inherit-product, vendor/gzosp/config/common_full_phone.mk)
+
+# Boot animation
+TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 1920
+
+## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := potter
-PRODUCT_NAME := full_potter
+PRODUCT_NAME := gzosp_potter
+PRODUCT_BRAND := motorola
+PRODUCT_MANUFACTURER := motorola
+
+PRODUCT_ENFORCE_RRO_TARGETS := \
+    framework-res
+
+PRODUCT_SYSTEM_PROPERTY_BLACKLIST := ro.product.model
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="Moto G5 Plus" \
+    DEVICE_MAINTAINERS="Nick van Bokhorst (GtrCraft)"
 
 # for specific
 $(call inherit-product, vendor/motorola/potter/potter-vendor.mk)
