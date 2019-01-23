295199e80cd364c5121eeb97139aed1037ad3f10
Vachounet <vachounet@live.fr>
2019-01-23 16:19:05 +0100

    potter: build sensor and audio service
    Change-Id: I3db36f74451a64b3c1e8eed4cce519891a71a8a2

--- device.mk
+++ device.mk
@@ -80,6 +80,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
+    android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.broadcastradio@1.0-impl
 
@@ -387,6 +388,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service \
     libsensorhub \
     motosh \
     sensorhub.msm8953 \
