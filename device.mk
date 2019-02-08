7aff1e0400d695c50e7ce92bf07e6bbf2e616d48
William Bellavnce <flintman@flintmancomputers.com>
2019-02-08 11:56:48 +0000

    Revert "potter: build sensor and audio service"
    This reverts commit 4db1425b7ff9bc909e4e3187f6b4401b63c286db.

--- device.mk
+++ device.mk
@@ -76,7 +76,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
-    android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.broadcastradio@1.0-impl
 
@@ -359,7 +358,6 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service \
     libsensorhub \
     motosh \
     sensorhub.msm8953 \
