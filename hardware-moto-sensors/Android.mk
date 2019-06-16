77b7045ab62fd8f2ed0381c35bc9f4b873e26e94
Jarl-Penguin <jarlpenguin@outlook.com>
2019-06-16 16:04:11 +0300

    potter: hardware-moto-sensors: proprietary --> vendor
    * This is an OSS HAL, not a proprietary one
    
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>
    Change-Id: I1881ffb5545b5c51397442b45920d49cd1da06a1

--- hardware-moto-sensors/Android.mk
+++ hardware-moto-sensors/Android.mk
@@ -182,7 +182,7 @@ ifeq ($(BOARD_USES_MOT_SENSOR_HUB), true)
         LOCAL_PRELINK_MODULE := false
         LOCAL_MODULE_RELATIVE_PATH := hw
         LOCAL_MODULE_TAGS := optional
-        LOCAL_PROPRIETARY_MODULE := true
+        LOCAL_VENDOR_MODULE := true
         LOCAL_SHARED_LIBRARIES += liblog libcutils libz libdl libutils
         LOCAL_CLANG := true
         LOCAL_MODULE := sensors.$(TARGET_BOARD_PLATFORM)
@@ -208,7 +208,7 @@ ifeq ($(BOARD_USES_MOT_SENSOR_HUB), true)
     LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
     LOCAL_SHARED_LIBRARIES := libcutils libc libutils liblog
-    LOCAL_PROPRIETARY_MODULE := true
+    LOCAL_VENDOR_MODULE := true
     LOCAL_MODULE := sensorhub.$(TARGET_BOARD_PLATFORM)
     LOCAL_MODULE_TAGS := optional
     LOCAL_CFLAGS += -Wno-gnu-designator -Wno-writable-strings
@@ -226,7 +226,7 @@ ifeq ($(BOARD_USES_MOT_SENSOR_HUB), true)
     LOCAL_MODULE_TAGS := optional
     LOCAL_CFLAGS := -DLOG_TAG=\"MOTOSH\" -DMODULE_$(SH_MODULE)
     LOCAL_MODULE := motosh
-    LOCAL_PROPRIETARY_MODULE := true
+    LOCAL_VENDOR_MODULE := true
     #LOCAL_CFLAGS+= -D_DEBUG
     LOCAL_CFLAGS += -Wall -Wextra
     LOCAL_CFLAGS += -Wno-gnu-designator -Wno-writable-strings
