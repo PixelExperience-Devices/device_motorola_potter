2d6998603d9ec9296326a637b3e0c0042e523097
kubersharma001 <kubersharma001@gmail.com>
2019-09-03 17:17:24 +0530

    potter: device.mk: set TARGET_COPY_OUT_VENDOR instead of system/vendor
     Also define ueventd.qcom.rc properly in rootdir/Android.mk
    
    Change-Id: I8eaf84b0769db684398c58c63b6b4571877cb8ed

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -80,3 +80,12 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES :=
 LOCAL_SHARED_LIBRARIES := libhidltransport
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := ueventd.qcom.rc
+LOCAL_MODULE_STEM  := ueventd.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/ueventd.qcom.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
+include $(BUILD_PREBUILT)
