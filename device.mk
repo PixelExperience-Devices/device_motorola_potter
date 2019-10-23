df35099071e5ce71006059be85bca0a3f6c6f013
kubersharma001 <kubersharma001@gmail.com>
2019-10-23 11:47:46 +0530

    potter: Build libcld80211
     * Needed for cnss-daemon and wfd
    
    Change-Id: I4206fdac39c5112a729f0de14324fd513601bb89

--- device.mk
+++ device.mk
@@ -388,6 +388,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
     hostapd \
+    libqsap_sdk \
+    libcld80211 \
     libwpa_client \
     wcnss_service \
     wificond \
