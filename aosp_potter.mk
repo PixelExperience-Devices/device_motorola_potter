17ef2a71ed020de9471bf45baa8be8a802d71764
ZJRDroid <priv.zjr@gmail.com>
2019-11-01 13:53:19 +0100

    [DNM] PixelExperience bringup
    Add new privapp permissions
    
    Don't use lzma

--- aosp_potter.mk
+++ aosp_potter.mk
@@ -21,7 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
 # Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from potter device
 $(call inherit-product, device/motorola/potter/device.mk)
@@ -29,10 +29,12 @@ $(call inherit-product, device/motorola/potter/device.mk)
 # Boot animation
 TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 1920
+TARGET_GAPPS_ARCH := arm64
+TARGET_BOOT_ANIMATION_RES := 1080
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := potter
-PRODUCT_NAME := arrow_potter
+PRODUCT_NAME := aosp_potter
 PRODUCT_BRAND := motorola
 PRODUCT_MANUFACTURER := motorola
 
