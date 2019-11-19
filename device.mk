184bf55e1fcac78c84ac3c19997c20275889e811
William Bellavnce <flintman@flintmancomputers.com>
2019-11-19 10:16:04 -0500

    bring back moto action
    Change-Id: Ib2eeaff23543a2bd42e5d3fc89e4a3ff3c512639

--- device.mk
+++ device.mk
@@ -256,8 +256,8 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.potter
 
 # MotoActions
-#PRODUCT_PACKAGES += \
-#    MotoActions
+PRODUCT_PACKAGES += \
+    MotoActions
 
 # Media
 PRODUCT_PACKAGES += \
