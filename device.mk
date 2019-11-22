cfebb1c0049c34969971877c1d50bb56582377cd
Mikhail Naganov <mnaganov@google.com>
2019-11-22 10:56:29 -0800

    potter: Switch to Audio HAL V6
    Bug: 141989952
    Test: atest VtsHalAudioV6_0TargetTest
          atest VtsHalAudioEffectV6_0TargetTest
                manual audio "smoke" test
    Change-Id: I41b5901beaac3a9c0b3de6ab9c2dc8deb0075e3b

--- device.mk
+++ device.mk
@@ -67,8 +67,8 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@4.0-impl \
-    android.hardware.audio.effect@4.0-impl \
+    android.hardware.audio@6.0-impl \
+    android.hardware.audio.effect@6.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.broadcastradio@1.0-impl \
     audio.primary.msm8953 \
