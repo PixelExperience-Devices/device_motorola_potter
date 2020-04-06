8706394853e369d1320eff05fafd23530d105de1
ZJRDroid <priv.zjr@gmail.com>
2020-04-06 17:07:16 -0300

    potter: update coral fingerprint to QQ2A.200405.005

--- init/init_potter.cpp
+++ init/init_potter.cpp
@@ -86,7 +86,7 @@ void vendor_load_properties()
 {
     // fingerprint
     property_override("ro.build.description", "potter-user 8.1.0 OPSS28.85-17-4 28698 release-keys");
-    property_override_triple("ro.build.fingerprint", "ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ2A.200305.003/6156912:user/release-keys");
+    property_override_triple("ro.build.fingerprint", "ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ2A.200405.005/6254899:user/release-keys");
 
     // sku
     std::string sku = "Moto G5 Plus (";
