d3afc29d9a79d3903447d64be8e4268c1544e391
ZJRDroid <priv.zjr@gmail.com>
2020-05-02 14:09:44 -0300

    Set device hostname on boot

--- device_props.mk
+++ device_props.mk
@@ -352,3 +352,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Fast App Launch
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.device_config.runtime_native.usap_pool_enabled=true
+
+# UI
+PRODUCT_PROPERTY_OVERRIDES += \
+    sys.use_fifo_ui=1
