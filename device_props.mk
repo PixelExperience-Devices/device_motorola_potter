9e7ba70637d944a12fbf20257812b32c84f7b349
ZJRDroid <priv.zjr@gmail.com>
2020-05-03 21:49:01 -0300

    set vendor.camera.aux.packagelist

--- device_props.mk
+++ device_props.mk
@@ -343,3 +343,6 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
     ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
 
+# Camera Aux Package list
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.camera.aux.packagelist=com.google.android.GoogleCamera,com.google.android.apps.cameraslite,org.codeaurora.snapcam
