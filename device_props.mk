ba58d1cd0fbf8206f71e93ab0aa8bc4c54e63a20
ZJRDroid <priv.zjr@gmail.com>
2020-06-05 10:33:26 -0300

    Add prop for fast app launch
    Change-Id: I5b1a77f1491098f6e46e97e5c27d9d996ef3ba81

--- device_props.mk
+++ device_props.mk
@@ -339,3 +339,6 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
     ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
 
+# Fast App Launch
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.device_config.runtime_native.usap_pool_enabled=true
