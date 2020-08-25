692300e6c0485bb1b38c6911596bd845ba50b2c2
Jarl-Penguin <jarlpenguin@outlook.com>
2020-08-26 01:22:56 +0300

    msm8937-common: Downrev radio to 1.1
    * 1.2 is unused
    
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: Ib03ee94168313b41ad8282e22953a2a9b79dd3d2

--- device.mk
+++ device.mk
@@ -349,7 +349,7 @@ PRODUCT_COPY_FILES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.2 \
+    android.hardware.radio@1.1 \
     android.hardware.radio.config@1.0 \
     android.hardware.secure_element@1.0 \
     librmnetctl \
