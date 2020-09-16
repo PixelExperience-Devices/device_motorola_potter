a8fc37e39bd165582179f00272703d872003254a
William Bellavnce <flintman@flintmancomputers.com>
2020-09-16 10:02:40 -0400

    hold off on SEPolicy for now  Only grab whats needed
    Change-Id: I01f1a13010a46ff1389e2e5a479e1a0b052f366e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -226,9 +226,10 @@ TARGET_USES_OLD_MNC_FORMAT := true
 TARGET_USES_ALTERNATIVE_MANUAL_NETWORK_SELECT := true
 
 # SELinux
-include device/qcom/sepolicy-legacy/sepolicy.mk
+#include device/qcom/sepolicy-legacy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+SELINUX_IGNORE_NEVERALLOWS := true
+#BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 
 # Sensor
 USE_SENSOR_MULTI_HAL := true
