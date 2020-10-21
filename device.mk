d9e77cc8a4b08922e1b961c12d4205aac4dd66d7
Arian <arian.kulmer@web.de>
2020-10-21 16:54:11 -0400

    Build new aidl qti power HAL
    - CAF converted their hidl power hal to aidl
    
    Change-Id: I4bdea354e54d34d0dfe9b811e228fc6f8762d2ed

--- device.mk
+++ device.mk
@@ -308,7 +308,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service
+    android.hardware.power-service-qti
 
 # Public Libraries
 PRODUCT_COPY_FILES += \
