f2fff18a5021901980adf18694e0217080a2d56b
William Bellavnce <flintman@flintmancomputers.com>
2020-10-26 06:01:00 -0400

    bring on device tree sepolicy repo and update path
    Change-Id: Ic50563c35fc2b1def0841748483fb2fca49260c0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -225,7 +225,7 @@ TARGET_USES_OLD_MNC_FORMAT := true
 TARGET_USES_ALTERNATIVE_MANUAL_NETWORK_SELECT := true
 
 # SELinux
-include device/qcom/sepolicy-legacy/sepolicy.mk
+include device/qcom/sepolicy-legacy/SEPolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 
