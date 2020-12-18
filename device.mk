7ce23be9f5c01732c6d769096255546b258300e4
zjunior06 <zjunior06@pixelexperience.org>
2020-12-18 11:39:35 -0300

    potter: Don't ship RadioFM

--- device.mk
+++ device.mk
@@ -182,12 +182,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1
 
-# FM
-PRODUCT_PACKAGES += \
-    FM2 \
-    libqcomfm_jni \
-    qcom.fmradio
-
 # Gatekeeper HAL
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
